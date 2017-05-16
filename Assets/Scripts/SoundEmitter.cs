1e79d5a9bc8d68bb127847dcc8af870814e5ebd4
Caillou <p.chapelet@rubika-edu.com>
2017-05-16 16:18:29 +0200

    caca

---
+++ Assets/Scripts/SoundEmitter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEmitter : MonoBehaviour {
+    public AudioClip Sound;
+    public float Portee;
+
+    private AudioSource source;
+    private Transform tf;
+
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+        tf = transform;
+    }
+
+    public void PlaySound()
+    {
+        //source.PlayOneShot(Sound);
+
+        float distance = Vector3.Distance(tf.position, Player.Instance.GetPosition());
+
+        if(distance <= Portee)
+        {
+            Player.Instance.SetDestination(tf.position);
+        }
+    }
+}
