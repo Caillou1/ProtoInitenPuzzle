af3d469ecb3b112662a0112d6c4ee30d01b340ef
Caillou <p.chapelet@rubika-edu.com>
2017-05-16 16:38:25 +0200

    Added Sound Emitter and Player Movements

--- Assets/Scripts/SoundEmitter.cs
+++ Assets/Scripts/SoundEmitter.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class SoundEmitter : MonoBehaviour {
     public AudioClip Sound;
-    public float Portee;
 
     private AudioSource source;
     private Transform tf;
+    private float portee;
 
     private void Start()
     {
         source = GetComponent<AudioSource>();
         tf = transform;
+        portee = GetComponent<SphereCollider>().radius;
     }
 
     public void PlaySound()
     {
-        //source.PlayOneShot(Sound);
-
         float distance = Vector3.Distance(tf.position, Player.Instance.GetPosition());
 
-        if(distance <= Portee)
+        if(distance <= portee)
         {
+            source.PlayOneShot(Sound, distance/portee);
             Player.Instance.SetDestination(tf.position);
         }
     }
