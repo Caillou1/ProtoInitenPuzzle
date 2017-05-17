a4c2ea80010870b5626fbdf232a3ae3f5d86a5ed
Caillou <p.chapelet@rubika-edu.com>
2017-05-17 09:56:49 +0200

    up

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour {
+    public SoundEmitter[] LinkedSoundEmitters;
+    public AudioClip SoundToPlay;
+    public Material Mat;
+
+	void Start () {
+		foreach(var emitter in LinkedSoundEmitters)
+        {
+            emitter.SetMaterial(Mat);
+        }
+	}
+
+    public void PlaySoundAtEmitters()
+    {
+        foreach(var emitter in LinkedSoundEmitters)
+        {
+            emitter.PlaySound(SoundToPlay);
+        }
+    }
+}
--- Assets/Scripts/SoundEmitter.cs
+++ Assets/Scripts/SoundEmitter.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SoundEmitter : MonoBehaviour {
-    public AudioClip Sound;
     public Material Mat;
 
     private AudioSource source;
@@ -17,13 +16,18 @@ public class SoundEmitter : MonoBehaviour {
         portee = GetComponent<SphereCollider>().radius;
     }
 
-    public void PlaySound()
+    public void SetMaterial(Material m)
+    {
+        GetComponent<MeshRenderer>().material = m;
+    }
+
+    public void PlaySound(AudioClip Sound)
     {
         float distance = Vector3.Distance(tf.position, Player.Instance.GetPosition());
 
         if(distance <= portee)
         {
-            var hits = Physics.RaycastAll(tf.position, (Player.Instance.GetPosition() - tf.position).normalized);
+            var hits = Physics.RaycastAll(tf.position, (Player.Instance.GetPosition() - tf.position).normalized, distance);
             bool canPlaySound = true;
 
             foreach(var hit in hits)
@@ -48,7 +52,7 @@ public class SoundEmitter : MonoBehaviour {
         {
             if(other.CompareTag("Animal"))
             {
-                var hits = Physics.RaycastAll(tf.position, (Player.Instance.GetPosition() - tf.position).normalized);
+                var hits = Physics.RaycastAll(tf.position, (Player.Instance.GetPosition() - tf.position).normalized, Vector3.Distance(tf.position, other.transform.position));
                 bool canPlaySound = true;
 
                 foreach (var hit in hits)
