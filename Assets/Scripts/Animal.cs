ec386d6e8b40c3a4bfcd90eea8e2c107d8b60ba8
Caillou <p.chapelet@rubika-edu.com>
2017-05-17 18:15:36 +0200

    fix animals

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -6,11 +6,13 @@ using UnityEngine.AI;
 public class Animal : MonoBehaviour {
     public bool ChasePlayer;
 
+    private Transform tf;
     private NavMeshAgent navMeshAgent;
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        tf = transform;
     }
 
     public void SetDestination(Vector3 Destination)
@@ -22,7 +24,23 @@ public class Animal : MonoBehaviour {
     {
         if (ChasePlayer && other.CompareTag("Player"))
         {
-            navMeshAgent.SetDestination(Player.Instance.GetPosition());
+            float distance = Vector3.Distance(tf.position, Player.Instance.GetPosition());
+            var hits = Physics.RaycastAll(tf.position, (Player.Instance.GetPosition() - tf.position).normalized, distance);
+            bool canAttack = true;
+
+            foreach (var hit in hits)
+            {
+                if (hit.collider.CompareTag("Wall"))
+                {
+                    canAttack = false;
+                    break;
+                }
+            }
+
+            if (canAttack)
+            {
+                navMeshAgent.SetDestination(Player.Instance.GetPosition());
+            }
         }
     }
 }
