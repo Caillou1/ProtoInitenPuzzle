912c187451ab5c8820fbc268360081fd8344f88c
Caillou <p.chapelet@rubika-edu.com>
2017-05-18 14:10:02 +0200

    update

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour {
+    public AudioClip[] AvailableSounds;
+
+    private Material[] AssociatedMaterials;
+
+    public static SaveManager Instance = null;
+
+	void Start () {
+		if(Instance == null)
+        {
+            Instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+	}
+
+    private void InitTab()
+    {
+        AssociatedMaterials = new Material[AvailableSounds.Length];
+        for(int i = 0; i< AssociatedMaterials.Length; i++)
+        {
+            AssociatedMaterials[i] = null;
+        }
+    }
+
+    public bool IsSet(AudioClip Sound)
+    {
+        return AssociatedMaterials[FindIndex(Sound)] != null;
+    }
+
+    private int FindIndex(AudioClip Sound)
+    {
+        int index = -1;
+
+        for (int i = 0; i < AvailableSounds.Length; i++)
+        {
+            if (AvailableSounds[i] == Sound)
+            {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+
+    public void SetMaterial(AudioClip Sound, Material Mat)
+    {
+        AssociatedMaterials[FindIndex(Sound)] = Mat;
+    }
+}
